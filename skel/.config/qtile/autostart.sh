7314f82e5732018033301c76995ac53cf8879cd1
sahanuser <sahan.user@gmail.com>
2023-01-01 13:31:47 +0530

    skel

--- skel/.config/qtile/autostart.sh
+++ skel/.config/qtile/autostart.sh
@@ -4,4 +4,5 @@ picom &
 nm-applet &
 nitrogen --restore &
 conky -c ~/.config/conky/conky.conf &
-/usr/lib/xfce-polkit/xfce-polkit &
+#/usr/lib/xfce-polkit/xfce-polkit &
+/usr/lib/polkit-kde-authentication-agent-1 &
